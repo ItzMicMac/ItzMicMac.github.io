fdcf68e27bac5552db1ebe4a632385f7bf7ab54a
McKinley Edwards <edwardsmack10@gmail.com>
2024-11-06 13:51:36 -0500

    Added some styles and dynamically placed buttons

---
+++ script.js
@@ -0,0 +1,27 @@
+const GAME_URLS = [
+    "https://ItzMicMac.github.io/MemoryMinigame",
+    "https://ItzMicMac.github.io/HLCrate",
+    "https://ItzMicMac.github.io/HLDes",
+    "https://ItzMicMac.github.io/HLCooking",
+    "https://ItzMicMac.github.io/CubicDrill"
+]
+const GAME_NAMES = [
+    "Prestige Memory Minigame",
+    "Highlife Crate Minigame",
+    "Highlife Des Minigame",
+    "Highlife Cooking Minigame",
+    "Cubic Drill Minigame"
+];
+
+function addButtons(){
+    const app_con = document.querySelector(".app-container");
+    for (let i = 0; i < GAME_NAMES.length; i++) {
+        const button = document.createElement("button");
+        button.classList.add("app-button");
+        button.innerHTML = GAME_NAMES[i];
+        button.onclick = () => {
+            window.location.href = GAME_URLS[i];
+        }
+        app_con.appendChild(button);
+    }
+}
